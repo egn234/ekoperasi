3e9d7f2a8bbfc3e6098bb5c34ef8b378e2c31d24
EggAnt <42627926+egn234@users.noreply.github.com>
2024-08-31 15:19:17 +0700

    cleaning

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,10 +37,13 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 
 $routes->get('/', 'Login::index');
-// $routes->group('test', static function ($routes){
-//     $routes->get('test_cicilan', 'Test_field::insert_cicilan');
-//     $routes->get('test_gen_cicilan', 'Test_field::gen_sisa_cicilan');
-// });
+
+//for testing db only, uncomment this route
+$routes->group('test', static function ($routes){
+    $routes->get('test_cicilan', 'Test_field::insert_cicilan');
+    $routes->get('test_gen_cicilan', 'Test_field::gen_sisa_cicilan');
+    $routes->get('test_db', 'Test_field::test_db');
+});
 
 $routes->get('registrasi', 'Register::index');
 
