7518b70a0300ff5aea8ef4bb4e9c2a46c2d11a96
EggAnt <42627926+egn234@users.noreply.github.com>
2024-12-12 09:21:24 +0700

    minor fix session

--- app/Filters/LoginFilter.php
+++ app/Filters/LoginFilter.php
@@ -9,31 +9,33 @@ class LoginFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-	    if(session()->get('logged_in')){
-	    	$flag = session()->get('flag');
-	    	$idgroup = session()->get('idgroup');
+        if (session()->has('logged_in') && session()->get('logged_in')) {
+            $flag = session()->get('flag');
+            $idgroup = session()->get('idgroup');
 
-	    	if ($flag == 0) {
-				session()->destroy();
-				return redirect()->to('/');
-	    	}
+            if ($flag === 0) {
+                session()->destroy();
+                return redirect()->to('/');
+            }
 
-	    	if ($idgroup == 1) {
-	    		return redirect()->to('admin/dashboard');
-	    	}
-
-	    	if ($idgroup == 2) {
-	    		return redirect()->to('bendahara/dashboard');
-	    	}
-
-	    	if ($idgroup == 3) {
-	    		return redirect()->to('ketua/dashboard');
-	    	}
-
-	    	if ($idgroup == 3) {
-	    		return redirect()->to('anggota/dashboard');
-	    	}
-	    }
+            switch ($idgroup) {
+                case 1:
+                    return redirect()->to('admin/dashboard');
+                    break;
+                case 2:
+                    return redirect()->to('bendahara/dashboard');
+                    break;
+                case 3:
+                    return redirect()->to('ketua/dashboard');
+                    break;
+                case 4:
+                    return redirect()->to('anggota/dashboard');
+                    break;
+                default:
+                    session()->destroy();
+                    return redirect()->to('/');
+            }
+        }
     }
 
     //--------------------------------------------------------------------
